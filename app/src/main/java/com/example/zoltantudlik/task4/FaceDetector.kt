fa52b930627f4f669c529b98de9f52702c2bdfb2
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-01 00:59:46 +0300

    add face detection

---
+++ app/src/main/java/com/example/zoltantudlik/task4/FaceDetector.kt
@@ -0,0 +1,57 @@
+package com.example.zoltantudlik.task4
+
+import android.graphics.Bitmap
+import android.util.Log
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+
+
+class FaceDetector(activity: MainActivity) {
+    private var detector: FirebaseVisionFaceDetector
+    private val treshold = 0.5f
+    private val mainActivity: MainActivity
+
+    init {
+        val options = FirebaseVisionFaceDetectorOptions.Builder()
+                .setModeType(FirebaseVisionFaceDetectorOptions.FAST_MODE)
+                .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                .setLandmarkType(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                .setTrackingEnabled(false)
+                .build()
+
+        mainActivity = activity
+        detector = FirebaseVision.getInstance().getVisionFaceDetector(options)
+    }
+
+    fun detectFaces(bitmap: Bitmap) {
+        val image: FirebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap)
+
+        detector.detectInImage(image)
+                .addOnSuccessListener { faces ->
+                    Log.d("MCC", "Detected faces: ${faces.size}")
+                    faces.forEach { face ->
+                        Log.d("MCC", "Smiling chance: ${face.smilingProbability}")
+                        Log.d("MCC", "Eyes left chance: ${face.rightEyeOpenProbability}")
+                        Log.d("MCC", "Eyes right chance: ${face.leftEyeOpenProbability}")
+                    }
+
+                    val smilingAvg = faces
+                            .map { face -> face.smilingProbability }
+                            .average()
+
+                    val eyesAvg = faces
+                            .map { face -> (face.leftEyeOpenProbability + face.rightEyeOpenProbability) / 2 }
+                            .average()
+
+                    val isSmiling = smilingAvg >= treshold
+                    val hasEyes = eyesAvg >= treshold
+
+                    mainActivity.setFaceInfo(faces.size, isSmiling, hasEyes)
+                }
+                .addOnFailureListener { error ->
+                    Log.e("MCC", "Error during face recognition", error)
+                }
+    }
+}
--- app/src/main/java/com/example/zoltantudlik/task4/MainActivity.kt
+++ app/src/main/java/com/example/zoltantudlik/task4/MainActivity.kt
@@ -1,12 +1,90 @@
 package com.example.zoltantudlik.task4
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pickPhotoButton: Button
+    private lateinit var barCodeTextView: TextView
+    private lateinit var peopleTextView: TextView
+    private lateinit var smileTextView: TextView
+    private lateinit var eyesTextView: TextView
+    private lateinit var imageView: ImageView
+
+    private val RESULT_LOAD_IMG = 1
+    private val MY_PERMISSIONS_READ_EXTERNAL_STORAGE = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pickPhotoButton = findViewById(R.id.btnPickPhoto)
+        barCodeTextView = findViewById(R.id.txtBarcode)
+        peopleTextView = findViewById(R.id.txtNumPeople)
+        smileTextView = findViewById(R.id.txtSmile)
+        eyesTextView = findViewById(R.id.txtEyes)
+        imageView = findViewById(R.id.imageView)
+
+        checkPermissions()
+
+        pickPhotoButton.setOnClickListener { v ->
+            loadImageFromGallery()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RESULT_LOAD_IMG && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImage = data.data
+            val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+
+            val cursor = contentResolver.query(selectedImage,
+                    filePathColumn, null, null, null)
+            cursor!!.moveToFirst()
+
+            val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+            val picturePath = cursor.getString(columnIndex)
+            cursor.close()
+
+            val image = BitmapFactory.decodeFile(picturePath)
+
+            imageView.setImageBitmap(image)
+
+            val faceDetector = FaceDetector(this)
+            faceDetector.detectFaces(image)
+        }
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), MY_PERMISSIONS_READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun loadImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, RESULT_LOAD_IMG)
+    }
+
+    fun setFaceInfo(faces: Int, isSmiling: Boolean, hasEyes: Boolean) {
+        barCodeTextView.text = getText(R.string.no)
+        peopleTextView.text = faces.toString()
+        smileTextView.text = if(isSmiling) getText(R.string.yes) else getText(R.string.no)
+        eyesTextView.text = if(hasEyes) getText(R.string.yes) else getText(R.string.no)
     }
 }
